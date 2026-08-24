27b52e723caee68537d0a4b8a1967dce425a2e2e
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-08-24 19:20:08 +0530

    classes

--- 03Purtes/dist/index.js
+++ 03Purtes/dist/index.js
@@ -1,5 +1,39 @@
 "use strict";
+// class User{
+//     public email:string
+//     name:string
+//     private readonly city:string="hyderabad"
+//     constructor(email:string,name:string){
+//         this.email=email;
+//         this.name=name;
+//     }
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("typescript is here");
-console.log("typescript is amazing");
+// const sravani=new User("srav@s.com","sravani");
+class User {
+    email;
+    name;
+    _coursecount = 1;
+    city = "hyderabad";
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get coursecount() {
+        return this._coursecount;
+    }
+    set coursecount(num) {
+        if (num <= 1) {
+            throw new Error("course count more than 1");
+        }
+        this._coursecount = num;
+    }
+}
+const sravani = new User("srav@s.com", "sravani");
 //# sourceMappingURL=index.js.map
--- 03Purtes/src/index.ts
+++ 03Purtes/src/index.ts
@@ -1,2 +1,40 @@
-console.log("typescript is here");
-console.log("typescript is amazing.....");
+// class User{
+//     public email:string
+//     name:string
+//     private readonly city:string="hyderabad"
+//     constructor(email:string,name:string){
+//         this.email=email;
+//         this.name=name;
+//     }
+// }
+
+// const sravani=new User("srav@s.com","sravani");
+
+class User{
+   private _coursecount=1
+    readonly city:string="hyderabad"
+    constructor(
+        public email:string,
+        public name:string,
+        // private userId:string
+    ){
+
+    }
+    private deleteToken(){
+        console.log("token deleted")
+    }
+    get getAppleEmail():string{
+      return `apple${this.email}`
+    }
+    get coursecount():number{
+        return this._coursecount
+    }
+    set coursecount(num){
+     if(num<=1){
+        throw new Error("course count more than 1")
+     }
+     this._coursecount=num
+    }
+}
+
+const sravani=new User("srav@s.com","sravani");
