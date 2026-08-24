5e1ef2bcf41f2eb8e05dd26a4088bc2f7e6c49e4
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-08-24 19:38:18 +0530

    interface use in classes

--- 03Purtes/dist/second.js
+++ 03Purtes/dist/second.js
@@ -1,3 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class instagram {
+    cameraMode;
+    filter;
+    burst;
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class youtube {
+    cameraMode;
+    filter;
+    burst;
+    short;
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+    createstory() {
+        console.log("Story was created");
+    }
+}
 //# sourceMappingURL=second.js.map
---
+++ 03Purtes/src/second.ts
@@ -0,0 +1,31 @@
+interface TakePhoto{
+    cameraMode:string
+    filter:string
+    burst:number
+}
+interface Story{
+    createstory():void
+}
+class instagram implements TakePhoto{
+    constructor(
+        public cameraMode:string,
+        public filter:string,
+        public burst:number 
+    ){
+
+    }
+}
+class youtube implements TakePhoto,Story{
+    constructor(
+        public cameraMode:string,
+        public filter:string,
+        public burst:number,
+        public short:string
+    ){
+
+    }
+    createstory(): void {
+        console.log("Story was created");
+        
+    }
+}
