833301f4a5831ddc12f69ec73d9e07eaf710c05c
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-08-26 12:09:55 +0530

    types of generics

--- 03Purtes/src/myGenerics.ts
+++ 03Purtes/src/myGenerics.ts
@@ -32,3 +32,38 @@ console.log(result); // 40
 const getMoreSearchproducts=<T>(products:T[]): T|undefined=>{
     return products[3]
 }
+
+interface Database{
+    connection:string,
+    username:string,
+    password:string
+}
+
+function anotherFunction<T,U extends Database>(valone:T,valTwo:U):Object{
+return {valone,valTwo}
+}
+
+// anotherFunction(3,{
+//     connection:"",
+//     username:"sravani",
+//     password:"KLrahul"
+// })
+
+//class types in generics
+
+interface quiz{
+    name:string,
+    type:string
+}
+interface course{
+    name:string,
+    author:string,
+    subject:string
+}
+
+class sellable<T>{
+public cart:T[]=[]
+addToCart(product:T){
+    this.cart.push(product)
+}
+}
